c9fa3f3bd1304d33118ef4976eefb76706958afc
necitboss <necitpresents@yandex.ru>
2025-04-09 01:01:58 +0300

    add output

--- src/create.js
+++ src/create.js
@@ -1,10 +1,18 @@
 
+const output = (body) => {
+	let string = body.name
+	body.body.forEach((element) => {
+		string += " << " + choice(element)
+	})
+	return string + ";"
+}
+
 const primitive_operator = (body) => {
-	// Здесь происходит обработка примитивных бинарных арифметических и логических операций (возможно, побитовых)
-	// Полный перечень:
-	// 1) арифметические: + - * / %
-	// 2) логические < > <= >= == && ||
-	return choice(body.first) + " " + body.operator + " " + choice(body.second)
+	/** Здесь происходит обработка примитивных бинарных арифметических и логических операций (возможно, побитовых)
+	* Полный перечень:
+	* 1) арифметические: + - * / %
+	* 2) логические < > <= >= == && || */
+	 return choice(body.first) + " " + body.operator + " " + choice(body.second)
 }
 
 // const var_using = (body) => {
@@ -12,7 +20,7 @@ const primitive_operator = (body) => {
 // }
 
 const var_declaration = (body) => {
-	// Здесь происходит обработка объявления переменных как с присваиванием, так и без
+	/** Здесь происходит обработка объявления переменных как с присваиванием, так и без */
 	if (!body.body) {
 		return body.value_type + " " + body.name + ";"
 	}
@@ -31,11 +39,16 @@ export const choice = (body) => {
 		switch (body.type){
 			case 'var_declaration':
 				return var_declaration(body);
+
 			case "var_using":
 				return body.name;
-				// return var_using(body);
+
 			case "primitive_operator":
 				return primitive_operator(body);
+
+			case "output":
+				return output(body);
+
 			default:
 				return body;
 		}
--- src/index.js
+++ src/index.js
@@ -4,9 +4,13 @@ import {choice} from "./create.js";
 
 const obj = {type: 'var_declaration', value_type: "int", name: "a", body: 5};
 const variable = {type: "var_using", name: "a"}
-const obj3 = {type: 'primitive_operator', operator: "+", first: -12, second: {type: "var_using", name: "a"}};
+const obj3 = {type: 'primitive_operator', operator: "+", first: 2, second: {type: "var_using", name: "a"}};
 const obj2 = {type: 'var_declaration', value_type: "int", name: "b", body: obj3};
+const obj4 = {type: 'output', name: "cout", body: ["Hello ", "world!", "\\n"]}
+const obj5 = {type: 'output', name: "cout", body: ["You clicked", obj3, "times!\\n"]}
 
 
 console.log(choice(obj))
 console.log(choice(obj2))
+console.log(choice(obj4))
+console.log(choice(obj5))
