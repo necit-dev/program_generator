ec485a6486014103a677034be7e73bb256e6670b
necitboss <necitpresents@yandex.ru>
2025-04-09 00:49:29 +0300

    add var_using and primitive operations

---
+++ src/create.js
@@ -0,0 +1,44 @@
+
+const primitive_operator = (body) => {
+	// Здесь происходит обработка примитивных бинарных арифметических и логических операций (возможно, побитовых)
+	// Полный перечень:
+	// 1) арифметические: + - * / %
+	// 2) логические < > <= >= == && ||
+	return choice(body.first) + " " + body.operator + " " + choice(body.second)
+}
+
+// const var_using = (body) => {
+// 	return body.name
+// }
+
+const var_declaration = (body) => {
+	// Здесь происходит обработка объявления переменных как с присваиванием, так и без
+	if (!body.body) {
+		return body.value_type + " " + body.name + ";"
+	}
+	let string = body.value_type + " " + body.name + " = ";
+	string += choice(body.body) + ";"
+	return string;
+}
+
+export const choice = (body) => {
+	if (typeof (body) === 'number' || typeof (body) === 'boolean') {
+		return body;
+	}else if (typeof body === 'string') {
+		return "\"" + body + "\""
+	}else if (typeof (body) === 'object' && body.hasOwnProperty('type')) {
+
+		switch (body.type){
+			case 'var_declaration':
+				return var_declaration(body);
+			case "var_using":
+				return body.name;
+				// return var_using(body);
+			case "primitive_operator":
+				return primitive_operator(body);
+			default:
+				return body;
+		}
+
+	}
+}
--- src/index.js
+++ src/index.js
@@ -1,26 +1,12 @@
+import {choice} from "./create.js";
 
-const var_declaration = (body) => {
-	let string = body.value_type + " " + body.name + " = ";
-	string += choice(body.body) + ";"
-	return string;
-}
 
-const choice = (body) => {
-	if (typeof (body) === 'number' || typeof (body) === 'string' || typeof (body) === 'boolean') {
-		return body;
-	}else if (typeof (body) === 'object' && body.hasOwnProperty('type')) {
-		switch (body.type){
-			case 'var_declaration':
-				return var_declaration(body);
-
-			default:
-				return body;
-		}
-	}
-}
 
 const obj = {type: 'var_declaration', value_type: "int", name: "a", body: 5};
-const obj2 = {type: 'var_declaration', value_type: "int", name: "b", body: 10};
+const variable = {type: "var_using", name: "a"}
+const obj3 = {type: 'primitive_operator', operator: "+", first: -12, second: {type: "var_using", name: "a"}};
+const obj2 = {type: 'var_declaration', value_type: "int", name: "b", body: obj3};
+
 
 console.log(choice(obj))
 console.log(choice(obj2))
