1d3dd7e543851ddb1354af2fb3f1c30b636c89d7
necitboss <necitpresents@yandex.ru>
2025-04-09 16:42:43 +0300

    add manipulator and var_assigning

--- src/create.js
+++ src/create.js
@@ -1,56 +1,73 @@
 
-const output = (body) => {
-	let string = body.name
-	body.body.forEach((element) => {
+const var_assigning = (elem) => {
+	/** Здесь происходит обработка присваивания значений переменных */
+	return elem.name + " = " + choice(elem.body) + ";"
+}
+
+const manipulator = (elem) => {
+	/** Здесь происходит обработка манипуляторов, таких как endl */
+	return elem.name;
+}
+
+const output = (elem) => {
+	/** Здесь происходит обработка потока вывода, в том числе cout */
+	let string = elem.name
+	elem.body.forEach((element) => {
 		string += " << " + choice(element)
 	})
 	return string + ";"
 }
 
-const primitive_operator = (body) => {
+const primitive_operator = (elem) => {
 	/** Здесь происходит обработка примитивных бинарных арифметических и логических операций (возможно, побитовых)
 	* Полный перечень:
 	* 1) арифметические: + - * / %
 	* 2) логические < > <= >= == && || */
-	 return choice(body.first) + " " + body.operator + " " + choice(body.second)
+	 return choice(elem.first) + " " + elem.operator + " " + choice(elem.second)
 }
 
 // const var_using = (body) => {
 // 	return body.name
 // }
 
-const var_declaration = (body) => {
+const var_declaration = (elem) => {
 	/** Здесь происходит обработка объявления переменных как с присваиванием, так и без */
-	if (!body.body) {
-		return body.value_type + " " + body.name + ";"
+	if (!elem.body) {
+		return elem.value_type + " " + elem.name + ";"
 	}
-	let string = body.value_type + " " + body.name + " = ";
-	string += choice(body.body) + ";"
+	let string = elem.value_type + " " + elem.name + " = ";
+	string += choice(elem.body) + ";"
 	return string;
 }
 
-export const choice = (body) => {
-	if (typeof (body) === 'number' || typeof (body) === 'boolean') {
-		return body;
-	}else if (typeof body === 'string') {
-		return "\"" + body + "\""
-	}else if (typeof (body) === 'object' && body.hasOwnProperty('type')) {
+export const choice = (elem) => {
+	if (typeof (elem) === 'number' || typeof (elem) === 'boolean') {
+		return elem;
+	}else if (typeof elem === 'string') {
+		return "\"" + elem + "\""
+	}else if (typeof (elem) === 'object' && elem.hasOwnProperty('type')) {
 
-		switch (body.type){
+		switch (elem.type){
 			case 'var_declaration':
-				return var_declaration(body);
+				return var_declaration(elem);
 
-			case "var_using":
-				return body.name;
+			case 'var_assigning':
+				return var_assigning(elem);
 
-			case "primitive_operator":
-				return primitive_operator(body);
+			case 'var_using':
+				return elem.name;
 
-			case "output":
-				return output(body);
+			case 'primitive_operator':
+				return primitive_operator(elem);
+
+			case 'output':
+				return output(elem);
+
+			case 'manipulator':
+				return manipulator(elem);
 
 			default:
-				return body;
+				return elem;
 		}
 
 	}
--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
 import {choice} from "./create.js";
 
-
-
 const obj = {type: 'var_declaration', value_type: "int", name: "a", body: 5};
 const variable = {type: "var_using", name: "a"}
 const obj3 = {type: 'primitive_operator', operator: "+", first: 2, second: {type: "var_using", name: "a"}};
 const obj2 = {type: 'var_declaration', value_type: "int", name: "b", body: obj3};
 const obj4 = {type: 'output', name: "cout", body: ["Hello ", "world!", "\\n"]}
-const obj5 = {type: 'output', name: "cout", body: ["You clicked", obj3, "times!\\n"]}
-
+const obj5 = {type: 'output', name: "cout", body: ["You clicked", obj3, "times!", {
+	type: 'manipulator', name: "endl"
+	}]}
+const obj6 = {
+	type: 'var_assigning',
+	name: 'b',
+	body: {
+		type: 'primitive_operator',
+		operator: "-",
+		first: {
+			type: "var_using",
+			name: "b"
+		},
+		second: 3
+	}
+}
 
 console.log(choice(obj))
 console.log(choice(obj2))
 console.log(choice(obj4))
 console.log(choice(obj5))
+console.log(choice(obj6))
